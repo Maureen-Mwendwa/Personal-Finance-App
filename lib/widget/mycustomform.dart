4fd0baece0c48c540a02084b12fccdc8e858683c
Maureen Mwendwa <mwendwaamaureen@gmail.com>
2024-05-18 18:56:22 +0300

    modified the create user account form to make it more visually appealing

--- lib/widget/mycustomform.dart
+++ lib/widget/mycustomform.dart
@@ -30,104 +30,194 @@ class MyCustomFormState extends State<MyCustomForm> {
   //Using a GlobalKey is the recommended way to access a form. Storing it as a variable allows it to be accessed at different points.
   Widget build(BuildContext context) {
     //Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          //TextFormFields and ElevatedButton with validation logic
-          //The TextFormField widget renders a material design text field
-          //and can display validation errors when they occur.
-          //Validate the input by providing a validator() function to the TextFormField.
-          //If the user's input isn't valid, the validator function returns a String containing an error message.
-          //If there are no errors, the validator must return null.
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Email'),
-            //The validator receives the text that the user has entered.
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              if (!EmailValidator.validate(value)) {
-                return 'Please enter a valid email address';
-              }
-              return null;
-            },
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background image
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'assets/SpendSenseLogo.jpeg'), // Replace with your image path
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-          TextFormField(
-            controller: _passwordController,
-            obscureText:
-                true, //usually for sensitive information like passwords. When set to true, it displays the text as hidden characters (commonly dots or asterisks) to prevent others from seeing it. Crucial for enhancing the security of user inputs in applications.
-            decoration: InputDecoration(labelText: 'Password'),
-            // validator: (value) {
-            //   if (value == null || value.isEmpty) {
-            //     return 'Please enter a password';
-            //   }
-            //   if (value.length < 8) {
-            //     return 'Password must be at least 8 characters long';
-            //   }
-            //   return null;
-            // },
-          ),
-          FlutterPwValidator(
-            controller: _passwordController,
-            minLength: 8,
-            uppercaseCharCount: 1,
-            lowercaseCharCount: 1,
-            numericCharCount: 1,
-            specialCharCount: 1,
-            width: 200,
-            height: 90,
-            defaultColor: Color(0xFFd3d3d3),
-            successColor: Color(0xFF2ee292),
-            failureColor: Color(0xFFf9433e),
-            onSuccess: () {
-              // success callback function
-              print('Password is valid');
-            },
-            onFail: () {
-              // failure callback function
-              print(
-                  'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.');
-            },
-          ),
-          TextFormField(
-            obscureText: true,
-            decoration: InputDecoration(labelText: 'Confirm Password'),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please re-enter your password';
-              }
-              // logic to compare with the password field
-              if (value != _passwordController.text) {
-                return 'Passwords do not match';
-              }
-              return null;
-            },
-          ),
-          //When the user attempts to submit the form, check if the form is valid.
-          //If it is, display a success message.
-          //If it isn't (the text field has no content) display the error message.
-          SizedBox(height: 100),
-          ElevatedButton(
-            onPressed: () {
-              //Validate returns true if the form is valid, or false otherwise.
-              if (_formKey.currentState!.validate()) {
-                //_formKey.currentState returns an instance of FormState?, which means it can either be a FormState object or null. The null assertion operator ! is used to assert that _formKey.currentState is not null before calling the validate() method on the FormState instance associated with the Form widget.
-                //If the form is valid, display a snackbar.
-                //In the real world, you'd often call a server or save the information in a database.
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                      content: Row(
-                    children: [
-                      Text('Details Submitted Successfully',
-                          style: TextStyle(color: Colors.green)),
-                      Icon(Icons.check_circle, color: Colors.green)
-                    ],
-                  )),
-                );
-              }
-            },
-            child: const Text('Submit', style: TextStyle(color: Colors.green)),
+          //Form Content
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  elevation: 6.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          //TextFormFields and ElevatedButton with validation logic
+                          //The TextFormField widget renders a material design text field
+                          //and can display validation errors when they occur.
+                          //Validate the input by providing a validator() function to the TextFormField.
+                          //If the user's input isn't valid, the validator function returns a String containing an error message.
+                          //If there are no errors, the validator must return null.
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                                borderSide:
+                                    BorderSide(color: Colors.blue, width: 2.0),
+                              ),
+                            ),
+                            //The validator receives the text that the user has entered.
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              if (!EmailValidator.validate(value)) {
+                                return 'Please enter a valid email address';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 16.0),
+                          TextFormField(
+                            controller: _passwordController,
+                            obscureText:
+                                true, //usually for sensitive information like passwords. When set to true, it displays the text as hidden characters (commonly dots or asterisks) to prevent others from seeing it. Crucial for enhancing the security of user inputs in applications.
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                                borderSide:
+                                    BorderSide(color: Colors.blue, width: 2.0),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter a password';
+                              }
+                              if (value.length < 8) {
+                                return 'Password must be at least 8 characters long';
+                              }
+                              if (!RegExp(r'[A-Z]').hasMatch(value)) {
+                                return 'Password must contain at least one uppercase letter';
+                              }
+                              if (!RegExp(r'[a-z]').hasMatch(value)) {
+                                return 'Password must contain at least one lowercase letter';
+                              }
+                              if (!RegExp(r'\d').hasMatch(value)) {
+                                return 'Password must contain at least one number';
+                              }
+                              if (!RegExp(r'[!@#\$&*~]').hasMatch(value)) {
+                                return 'Password must contain at least one special character';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 16.0),
+                          FlutterPwValidator(
+                            controller: _passwordController,
+                            minLength: 8,
+                            uppercaseCharCount: 1,
+                            lowercaseCharCount: 1,
+                            numericCharCount: 1,
+                            specialCharCount: 1,
+                            width: 200,
+                            height: 90,
+                            defaultColor: Color(0xFFd3d3d3),
+                            successColor: Color(0xFF2ee292),
+                            failureColor: Color(0xFFf9433e),
+                            onSuccess: () {
+                              // success callback function
+                              print('Password is valid');
+                            },
+                            onFail: () {
+                              // failure callback function
+                              print(
+                                  'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.');
+                            },
+                          ),
+                          SizedBox(height: 16.0),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Confirm Password',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                                borderSide:
+                                    BorderSide(color: Colors.blue, width: 2.0),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please re-enter your password';
+                              }
+                              // logic to compare with the password field
+                              if (value != _passwordController.text) {
+                                return 'Passwords do not match';
+                              }
+                              return null;
+                            },
+                          ),
+                          //When the user attempts to submit the form, check if the form is valid.
+                          //If it is, display a success message.
+                          //If it isn't (the text field has no content) display the error message.
+                          SizedBox(height: 32.0),
+                          ElevatedButton(
+                            onPressed: () {
+                              //Validate returns true if the form is valid, or false otherwise.
+                              if (_formKey.currentState!.validate()) {
+                                //_formKey.currentState returns an instance of FormState?, which means it can either be a FormState object or null. The null assertion operator ! is used to assert that _formKey.currentState is not null before calling the validate() method on the FormState instance associated with the Form widget.
+                                //If the form is valid, display a snackbar.
+                                //In the real world, you'd often call a server or save the information in a database.
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Row(
+                                    children: [
+                                      Text('Details Submitted Successfully',
+                                          style:
+                                              TextStyle(color: Colors.green)),
+                                      Icon(Icons.check_circle,
+                                          color: Colors.green)
+                                    ],
+                                  )),
+                                );
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.white,
+                              backgroundColor: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              padding: EdgeInsets.symmetric(vertical: 16.0),
+                            ),
+                            child: const Text('Submit',
+                                style: TextStyle(color: Colors.black)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
