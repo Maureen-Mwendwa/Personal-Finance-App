bcd97f587e51d63cb427c5d972dfaf04777394ce
Maureen Mwendwa <mwendwaamaureen@gmail.com>
2024-05-26 20:28:42 +0300

    Added Comments to the model

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,3 +1,7 @@
+//models are the fundamental building blocks representing the data in the application. They define the structure of the data and ensure that it is organized and accessible in a consistent manner.
+//Represents a primary expense category.
+//Contains a list of subcategories.
+//This structure allows easy grouping and retrieval of expenses under a specific category and sub category.
 class Category {
   final String name;
   final List<String> subcategories;
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,3 +1,6 @@
+//represents an individual expense entry.
+//Contains details such as the category, subcategory, item name, amount and date.
+//This structure provides a comprehensive representation of each expense, making it easier to perform operations like tracking, editing, and deleting expenses.
 class Expense {
   final String category;
   final String subcategory;
