c20402232c82158891b1e7bf41cbacd71f51367a
Maureen Mwendwa <mwendwaamaureen@gmail.com>
2024-05-28 22:02:55 +0300

    Defined a list of colors (_categoryColors) to be used for different categories. Created a function (getCategoryColors()) to generate a list of colors based on the number of categories, cycling through the predefined colors. Assigned colors to the BarChartRodData based on the category index. Ensured consistent padding and spacing. Fixed heights for charts to ensure no overlap and better visual separation.

--- lib/screens/analysis_screen.dart
+++ lib/screens/analysis_screen.dart
@@ -11,6 +11,15 @@ class AnalyticsScreen extends StatefulWidget {
 class _AnalyticsScreenState extends State<AnalyticsScreen> {
   String _selectedFilter = 'Month'; // Default filter
 
+  final List<Color> _categoryColors = [
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.orange,
+    Colors.purple,
+    Colors.brown,
+  ];
+
   @override
   Widget build(BuildContext context) {
     final expensesProvider = Provider.of<ExpensesProvider>(context);
@@ -38,125 +47,186 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
       );
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Analytics'),
-      ),
-      body: Column(
-        children: [
-          // Filter Dropdown
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: DropdownButton<String>(
-              value: _selectedFilter,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedFilter = newValue!;
-                });
-              },
-              items: <String>['Day', 'Month', 'Year']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
+    List<Color> getCategoryColors() {
+      return List.generate(
+        categoryExpenses.length,
+        (index) => _categoryColors[index % _categoryColors.length],
+      );
+    }
 
-          // Category Expenses Bar Chart
-          Text(
-            'Category Expenses',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Expanded(
-            child: BarChart(
-              BarChartData(
-                alignment: BarChartAlignment.spaceAround,
-                barGroups: categoryExpenses.entries.map((entry) {
-                  return BarChartGroupData(
-                    x: entry.key.hashCode,
-                    barRods: [
-                      BarChartRodData(
-                        toY: entry.value, // Updated toY
-                        color: Colors.blue,
-                      ),
-                    ],
-                    showingTooltipIndicators: [0],
-                  );
-                }).toList(),
-                titlesData: FlTitlesData(
-                  leftTitles:
-                      AxisTitles(sideTitles: SideTitles(showTitles: true)),
-                  bottomTitles: AxisTitles(
-                    sideTitles: SideTitles(
-                      showTitles: true,
-                      getTitlesWidget: (double value, TitleMeta meta) {
-                        final category = categoryExpenses.keys.firstWhere(
-                            (key) => key.hashCode == value.toInt(),
-                            orElse: () => '');
-                        return SideTitleWidget(
-                          child: Text(category),
-                          axisSide: meta.axisSide,
-                        );
-                      },
-                    ),
-                  ),
+    final categoryColors = getCategoryColors();
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0), // Add padding around the body
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment
+                .start, // Align children to the start of the column
+            children: [
+              // Filter Dropdown
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: DropdownButton<String>(
+                  value: _selectedFilter,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedFilter = newValue!;
+                    });
+                  },
+                  items: <String>['Day', 'Month', 'Year']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  isExpanded: true, // Make the dropdown expand to full width
                 ),
               ),
-            ),
-          ),
-
-          SizedBox(height: 20),
-
-          // Spending Trend Line Chart
-          Text(
-            'Spending Trend',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Expanded(
-            child: LineChart(
-              LineChartData(
-                lineBarsData: [
-                  LineChartBarData(
-                    spots: dateExpenses.entries.map((entry) {
-                      return FlSpot(
-                        entry.key.millisecondsSinceEpoch.toDouble(),
-                        entry.value,
+              // Category Expenses Bar Chart
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text(
+                  'Category Expenses',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 300, // Fixed height for the chart
+                child: BarChart(
+                  BarChartData(
+                    alignment: BarChartAlignment.spaceAround,
+                    barGroups: categoryExpenses.entries.map((entry) {
+                      final index =
+                          categoryExpenses.keys.toList().indexOf(entry.key);
+                      final color = categoryColors[index];
+                      return BarChartGroupData(
+                        x: entry.key.hashCode,
+                        barRods: [
+                          BarChartRodData(
+                            toY: entry.value, // Updated toY
+                            color: color,
+                          ),
+                        ],
+                        showingTooltipIndicators: [0],
                       );
                     }).toList(),
-                    isCurved: true,
-                    color: Colors.green,
-                  ),
-                ],
-                titlesData: FlTitlesData(
-                  bottomTitles: AxisTitles(
-                    sideTitles: SideTitles(
-                      showTitles: true,
-                      getTitlesWidget: (double value, TitleMeta meta) {
-                        final date =
-                            DateTime.fromMillisecondsSinceEpoch(value.toInt());
-                        final title = _selectedFilter == 'Day'
-                            ? '${date.day}/${date.month}'
-                            : _selectedFilter == 'Month'
-                                ? '${date.month}/${date.year}'
-                                : '${date.year}';
-                        return SideTitleWidget(
-                          child: Text(title),
-                          axisSide: meta.axisSide,
-                        );
-                      },
+                    titlesData: FlTitlesData(
+                      leftTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 28,
+                          getTitlesWidget: (double value, TitleMeta meta) {
+                            return Text(
+                              value.toString(),
+                              style: TextStyle(fontSize: 10),
+                            );
+                          },
+                        ),
+                      ),
+                      bottomTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          getTitlesWidget: (double value, TitleMeta meta) {
+                            final category = categoryExpenses.keys.firstWhere(
+                                (key) => key.hashCode == value.toInt(),
+                                orElse: () => '');
+                            return SideTitleWidget(
+                              axisSide: meta.axisSide,
+                              child: Text(category,
+                                  style: TextStyle(fontSize: 10)),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    gridData:
+                        FlGridData(show: false), // Hide grid lines for clarity
+                    borderData: FlBorderData(
+                      show: false, // Hide chart borders
                     ),
                   ),
-                  leftTitles:
-                      AxisTitles(sideTitles: SideTitles(showTitles: true)),
                 ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Divider(), // Add a divider for better visual separation
+              ),
+              // Spending Trend Line Chart
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text(
+                  'Spending Trend',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 300, // Fixed height for the chart
+                child: LineChart(
+                  LineChartData(
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: dateExpenses.entries.map((entry) {
+                          return FlSpot(
+                            entry.key.millisecondsSinceEpoch.toDouble(),
+                            entry.value,
+                          );
+                        }).toList(),
+                        isCurved: true,
+                        color: Colors.green,
+                        barWidth: 2,
+                        belowBarData: BarAreaData(
+                          show: true,
+                          color: Colors.green.withOpacity(0.3),
+                        ),
+                      ),
+                    ],
+                    titlesData: FlTitlesData(
+                      bottomTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 22,
+                          getTitlesWidget: (double value, TitleMeta meta) {
+                            final date = DateTime.fromMillisecondsSinceEpoch(
+                                value.toInt());
+                            final title = _selectedFilter == 'Day'
+                                ? '${date.day}/${date.month}'
+                                : _selectedFilter == 'Month'
+                                    ? '${date.month}/${date.year}'
+                                    : '${date.year}';
+                            return SideTitleWidget(
+                              axisSide: meta.axisSide,
+                              child:
+                                  Text(title, style: TextStyle(fontSize: 10)),
+                            );
+                          },
+                        ),
+                      ),
+                      leftTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 28,
+                          getTitlesWidget: (double value, TitleMeta meta) {
+                            return Text(
+                              value.toString(),
+                              style: TextStyle(fontSize: 10),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    gridData:
+                        FlGridData(show: false), // Hide grid lines for clarity
+                    borderData: FlBorderData(
+                      show: false, // Hide chart borders
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
