0d922ac03f40529689fdf687593c8fd3787cbd0a
Maureen Mwendwa <mwendwaamaureen@gmail.com>
2024-05-31 17:41:44 +0300

    Displayed Date alongside the cost price, selling price, quantity, and remaining quantity. Included edit button as well to enable the user update products.

--- lib/screens/product_list_page.dart
+++ lib/screens/product_list_page.dart
@@ -53,17 +53,25 @@ class ProductListPage extends StatelessWidget {
                     return ListTile(
                       title: Text(product.name),
                       subtitle: Text(
-                        'Cost: \$${product.costPrice}, Selling: \$${product.initialSellingPrice}, Initial Qty: ${product.initialQuantity}, Remaining: ${product.remainingQuantity}',
+                        'Cost: \$${product.costPrice}, Selling: \$${product.initialSellingPrice}, Initial Qty: ${product.initialQuantity}, Remaining: ${product.remainingQuantity}, Date: ${DateFormat.yMMMd().format(product.date)}',
                       ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          provider.deleteProduct(product.name);
-                        },
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              _showEditProductModal(context, provider, product);
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              provider.deleteProduct(product.name);
+                            },
+                          ),
+                        ],
                       ),
-                      onTap: () {
-                        _showEditProductModal(context, product);
-                      },
                     );
                   },
                 ),
@@ -92,12 +100,91 @@ class ProductListPage extends StatelessWidget {
     );
   }
 
-  void _showEditProductModal(BuildContext context, Product product) {
+  void _showEditProductModal(
+      BuildContext context, ProductProvider provider, Product product) {
+    final nameController = TextEditingController(text: product.name);
+    final costPriceController =
+        TextEditingController(text: product.costPrice.toString());
+    final sellingPriceController =
+        TextEditingController(text: product.initialSellingPrice.toString());
+    final quantityController =
+        TextEditingController(text: product.initialQuantity.toString());
+    DateTime selectedDate = product.date;
+
     showDialog(
       context: context,
       builder: (context) {
-        return Center(
-          child: ProductModal(product: product),
+        return AlertDialog(
+          title: Text('Edit Product ${product.name}'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(labelText: 'Product Name'),
+                ),
+                TextField(
+                  controller: costPriceController,
+                  decoration: InputDecoration(labelText: 'Cost Price'),
+                  keyboardType: TextInputType.number,
+                ),
+                TextField(
+                  controller: sellingPriceController,
+                  decoration: InputDecoration(labelText: 'Selling Price'),
+                  keyboardType: TextInputType.number,
+                ),
+                TextField(
+                  controller: quantityController,
+                  decoration: InputDecoration(labelText: 'Initial Quantity'),
+                  keyboardType: TextInputType.number,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Select Date:',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    DateTime? picked = await showDatePicker(
+                      context: context,
+                      initialDate: selectedDate,
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2101),
+                    );
+                    if (picked != null && picked != selectedDate)
+                      selectedDate = picked;
+                  },
+                  child: Text("Select Date"),
+                ),
+                Text(
+                  "${DateFormat.yMMMd().format(selectedDate)}",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                provider.updateProduct(
+                  product,
+                  nameController.text,
+                  double.parse(costPriceController.text),
+                  double.parse(sellingPriceController.text),
+                  int.parse(quantityController.text),
+                  selectedDate,
+                );
+                Navigator.of(context).pop();
+              },
+              child: Text('Update Product'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Cancel'),
+            ),
+          ],
         );
       },
     );
