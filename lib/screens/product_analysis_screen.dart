872f258a0b92b1a0d2e9a781125f3f80b06ccd4b
Maureen Mwendwa <mwendwaamaureen@gmail.com>
2024-06-07 23:41:44 +0300

    Modified the UI i.e added padding around the column, used sizedbox between text and charts, changed text and used CrossAxisAlignment.start to align text and charts to the start of the column creating a more organized and aligned layout.

--- lib/screens/product_analysis_screen.dart
+++ lib/screens/product_analysis_screen.dart
@@ -1,39 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
 import 'package:spendsense/screens/profit_loss_bar_chart.dart';
 import 'package:spendsense/screens/revenue_pie_chart.dart';
-import 'package:spendsense/screens/sales_line_graph.dart';
+import 'package:spendsense/screens/sales_bar_graph.dart';
 import 'package:spendsense/statemanagement/product_provider.dart';
 
-class VisualizationPage extends StatelessWidget {
+class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-            'Visualizations - ${DateFormat.yMMMd().format(DateTime.now())}'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 300,
-              child: ProfitLossBarChart(provider.products),
+      appBar: AppBar(title: const Text('Dashboard')),
+      body: Consumer<ProductProvider>(
+        builder: (context, provider, child) {
+          //Retrieve data from the provider
+          final dailySalesData = provider.getDailySalesData();
+          final productRevenueData = provider.getProductRevenueData();
+          final profitLossData = provider.getProfitLossData();
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Total Revenue From All Sales By Day',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  DailySalesChart(data: dailySalesData),
+                  SizedBox(height: 20),
+                  Text(
+                    'Product Revenue Breakdown For Each Product',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  ProductRevenueChart(data: productRevenueData),
+                  SizedBox(height: 20),
+                  Text(
+                    'Profit/Loss',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  ProfitLossChart(data: profitLossData),
+                ],
+              ),
             ),
-            SizedBox(height: 20),
-            SizedBox(
-              height: 300,
-              child: SalesLineGraph(provider.sales, provider.products),
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-              height: 300,
-              child: RevenuePieChart(provider.products, provider.sales),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
