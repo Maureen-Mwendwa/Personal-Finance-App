efda364f53b4ecd6d75c7893876b75cb842b9bdb
Maureen Mwendwa <mwendwaamaureen@gmail.com>
2024-06-10 18:39:48 +0300

    Added firebase authentication to create a user with email and password, added codes that writes to the firestore database after creating the user, added password visibility toggle and called the registerUser function in the Register button

--- lib/widget/mycustomform.dart
+++ lib/widget/mycustomform.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter_pw_validator/flutter_pw_validator.dart';
@@ -21,10 +23,62 @@ class MyCustomFormState extends State<MyCustomForm> {
   GlobalKey<FormState> get formKey => _formKey;
   final _passwordController =
       TextEditingController(); //creating an instance of the TextEditingController class using TextEditingController() constructor and assigning it to the variable _passwordController which is private to indicate that it is accessible only within the current class or file. TextEditingController is used to manage the text input in text fields. It provides various properties and methods to interact with the text, such as retrieving the current text value, setting the text value, and adding listeners to monitor text changes. By creating an instance of TextEditingController, you can associate it with a TextField or TextFormField widget using the controller property. This allows you to access and control the text input programmatically. i.e. TextField(controller: _passwordController). By doing this, you can access the text value entered by the user using _passwordController.text, set the initial text value using _passwordController.text = 'initial value', or listen to text changes using _passwordController.addListener((){/*handle text changes*/}. NB: TextEditingController instances should be disposed of when they are no longer needed to avoid memory leaks.
+  final _emailController = TextEditingController();
+  bool _passwordVisible = false;
+  bool _confirmPasswordVisible = false;
+
   @override
   void dispose() {
     _passwordController.dispose();
+    _emailController.dispose();
     super.dispose();
+  }
+
+  //_formKey.currentState returns an instance of FormState?, which means it can either be a FormState object or null. The null assertion operator ! is used to assert that _formKey.currentState is not null before calling the validate() method on the FormState instance associated with the Form widget.  //Validate returns true if the form is valid, or false otherwise.
+
+  Future<void> _registerUser() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        //Create User with email and password
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(
+                email: _emailController.text,
+                password: _passwordController.text);
+        //Add user details to Firestore
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredential.user!.uid)
+            .set({
+          'Email': _emailController.text,
+          'Password': _passwordController.text
+        }); //Store hashed password in prectice
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Row(
+            children: [
+              Text('Details Submitted Successfully!',
+                  style: TextStyle(color: Colors.green)),
+              Icon(Icons.check_circle, color: Colors.green),
+            ],
+          )),
+        );
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('The password provided is too weak.')),
+          );
+        } else if (e.code == 'email-already-in-use') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text('The account already exists for that email.')),
+          );
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('An error occurred. Please try again.')),
+        );
+      }
+    }
   }
 
   //Using a GlobalKey is the recommended way to access a form. Storing it as a variable allows it to be accessed at different points.
@@ -67,6 +121,7 @@ class MyCustomFormState extends State<MyCustomForm> {
                           //If the user's input isn't valid, the validator function returns a String containing an error message.
                           //If there are no errors, the validator must return null.
                           TextFormField(
+                            controller: _emailController,
                             decoration: InputDecoration(
                               labelText: 'Email',
                               border: OutlineInputBorder(
@@ -93,7 +148,7 @@ class MyCustomFormState extends State<MyCustomForm> {
                           TextFormField(
                             controller: _passwordController,
                             obscureText:
-                                true, //usually for sensitive information like passwords. When set to true, it displays the text as hidden characters (commonly dots or asterisks) to prevent others from seeing it. Crucial for enhancing the security of user inputs in applications.
+                                !_passwordVisible, //usually for sensitive information like passwords. When set to true, it displays the text as hidden characters (commonly dots or asterisks) to prevent others from seeing it. Crucial for enhancing the security of user inputs in applications.
                             decoration: InputDecoration(
                               labelText: 'Password',
                               border: OutlineInputBorder(
@@ -103,6 +158,16 @@ class MyCustomFormState extends State<MyCustomForm> {
                                 borderRadius: BorderRadius.circular(12.0),
                                 borderSide:
                                     BorderSide(color: Colors.blue, width: 2.0),
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(_passwordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: () {
+                                  setState(() {
+                                    _passwordVisible = !_passwordVisible;
+                                  });
+                                },
                               ),
                             ),
                             validator: (value) {
@@ -152,7 +217,7 @@ class MyCustomFormState extends State<MyCustomForm> {
                           ),
                           SizedBox(height: 16.0),
                           TextFormField(
-                            obscureText: true,
+                            obscureText: !_confirmPasswordVisible,
                             decoration: InputDecoration(
                               labelText: 'Confirm Password',
                               border: OutlineInputBorder(
@@ -162,6 +227,17 @@ class MyCustomFormState extends State<MyCustomForm> {
                                 borderRadius: BorderRadius.circular(12.0),
                                 borderSide:
                                     BorderSide(color: Colors.blue, width: 2.0),
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(_confirmPasswordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: () {
+                                  setState(() {
+                                    _confirmPasswordVisible =
+                                        !_confirmPasswordVisible;
+                                  });
+                                },
                               ),
                             ),
                             validator: (value) {
@@ -180,26 +256,7 @@ class MyCustomFormState extends State<MyCustomForm> {
                           //If it isn't (the text field has no content) display the error message.
                           SizedBox(height: 32.0),
                           ElevatedButton(
-                            onPressed: () {
-                              //Validate returns true if the form is valid, or false otherwise.
-                              if (_formKey.currentState!.validate()) {
-                                //_formKey.currentState returns an instance of FormState?, which means it can either be a FormState object or null. The null assertion operator ! is used to assert that _formKey.currentState is not null before calling the validate() method on the FormState instance associated with the Form widget.
-                                //If the form is valid, display a snackbar.
-                                //In the real world, you'd often call a server or save the information in a database.
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content: Row(
-                                    children: [
-                                      Text('Details Submitted Successfully',
-                                          style:
-                                              TextStyle(color: Colors.green)),
-                                      Icon(Icons.check_circle,
-                                          color: Colors.green)
-                                    ],
-                                  )),
-                                );
-                              }
-                            },
+                            onPressed: _registerUser,
                             style: ElevatedButton.styleFrom(
                               foregroundColor: Colors.white,
                               backgroundColor: Colors.green,
@@ -208,7 +265,7 @@ class MyCustomFormState extends State<MyCustomForm> {
                               ),
                               padding: EdgeInsets.symmetric(vertical: 16.0),
                             ),
-                            child: const Text('Submit',
+                            child: const Text('Register',
                                 style: TextStyle(color: Colors.black)),
                           ),
                         ],
